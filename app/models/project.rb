b684e94e9bb12ffd070c6ae18748ae4a81e5cea5
Riccardo Cambiassi <r.cambiassi@gmail.com>
2014-02-17 22:37:19 +0000

    resourcify Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,8 @@ class Project < ActiveRecord::Base
   after_validation :geocode, if: -> (obj) {
     obj.location.present? && obj.location_changed? }
 
+  resourcify
+
   has_many :properties
   has_many :memberships
   has_many :members, source: :user, through: :memberships
