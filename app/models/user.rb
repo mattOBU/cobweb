42e6f94a878e642d8a142741ce87f5baff9e38d0
Riccardo Cambiassi <r.cambiassi@gmail.com>
2014-02-17 22:37:48 +0000

    Project#role_for(resource)

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,18 @@ class User < ActiveRecord::Base
   has_many :projects, through: :memberships
   has_many :properties, through: :projects
 
-  def level
-    "admin"
-  end
-
   def name_or_email
     name || email
+  end
+
+  def member_of?(project)
+    projects.include? project
+  end
+
+  def role_for(resource)
+    roles.
+      where(resource_type: resource.class.name, resource_id: resource.id).
+      pluck(:name).
+      first
   end
 end
