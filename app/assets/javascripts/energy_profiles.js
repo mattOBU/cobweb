c0761baf80bf277fb9803196d9234d3e2f39ac1e
Riccardo Cambiassi <r.cambiassi@gmail.com>
2014-03-12 14:34:47 +0000

    first stab at the autosave logic for energy profiles

--- app/assets/javascripts/energy_profiles.js
+++ app/assets/javascripts/energy_profiles.js
@@ -1,3 +1,26 @@
+var saveDirtyForm = function(el) {
+  var thisForm = $(el).parents('form');
+  var currentProfileType = thisForm.data('profile-type');
+  var newProfileType = $(el).val();
+
+  if (currentProfileType != newProfileType) {
+    thisForm.data('switch-to', newProfileType);
+    if (thisForm.data('dirty')) {
+      //$('#confirm-modal').modal({ backdrop: 'static', keyboard: false});
+      console.log("Saving form...");
+      thisForm.submit();
+    }
+  }
+}
+
+var setDirty = function(el) {
+    // set the current form as dirty
+    var thisForm = $(el).parents('form')
+    var currentProfileType = thisForm.children('select[name="building_energy_profile[profile_type]"]').val();
+    console.log("Marking form as dirty");
+    thisForm.data('dirty', true);
+    thisForm.data('profile-type', currentProfileType);
+}
 
 jQuery(document).ready(function($) {
   // page setup
@@ -8,8 +31,25 @@ jQuery(document).ready(function($) {
     $(this).find('input').tab('show');
   });
 
+  // keep track of changes
+  $('input[type=text]').on('keypress', function() {
+    setDirty(this);
+  });
+
+  $('select[name="building_energy_profile[profile_type]"]').on('change', function() {
+    saveDirtyForm(this);
+  });
+
+  $('form.new_building_energy_profile').on('ajax:success', function(e, data, status, xhr) {
+    console.log("Received ajax response");
+    console.log("Energy Profile " + data["profile_type"]);
+  }).bind('ajax:error', function(e,xhr,status,error) {
+    console.log("Received ajax error");
+  });
+
+
   // by default, select 'yearly' granularity
-  if ($('input[name="building_energy_profile[granularity]"]:checked').length == 0) {
+  if ($('input[name=granularity]:checked').length == 0) {
     $('#granularities .btn:first').click();
   }
 });
