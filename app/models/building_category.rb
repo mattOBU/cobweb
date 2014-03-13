902eacfec4a6027969ebed039281d328bacb9e1b
Riccardo Cambiassi <r.cambiassi@gmail.com>
2014-03-13 00:50:34 +0000

    Better BuildingCategory representation

--- app/models/building_category.rb
+++ app/models/building_category.rb
@@ -1,30 +1,41 @@
 class BuildingCategory
   # TODO complete categories
   CATEGORIES= [
-    [ "single-family", "residential"     ],
-    [ "multi-family",  "residential"     ],
-    [ "office",        "non-residential" ],
-    [ "retail",        "non-residential" ],
-    [ "hotel",         "non-residential" ]
+    # slug, sector, description
+    [ "single-family", "residential", "Single Family"],
+    [ "multi-family",  "residential", "Multi Family"],
+    [ "office", "non-residential", "Office" ],
+    [ "hotel", "non-residential", "Hotel" ],
+    [ "storage", "non-residential", "Warehouse/storage" ],
+    [ "mercantile", "non-residential", "Mercantile/service" ],
+    [ "food-service", "non-residential", "Food service" ],
+    [ "entertainment", "non-residential", "Entertainment" ],
+    [ "education", "non-residential", "Education" ],
+    [ "healthcare", "non-residential", "Healthcare" ],
+    [ "public-order", "non-residential", "Public order" ],
+    [ "public-assembly", "non-residential", "Public assembly" ],
+    [ "institutional-lodging", "non-residential", "Institutional lodging" ],
+    [ "other-institutional", "non-residential", "Other institutional" ],
+    [ "mixed-use", "non-residential", "Mixed-use buildings" ],
+    [ "other-commercial", "non-residential", "Other commercial" ],
   ]
 
-  def initialize(slug, sector)
+  attr_reader :slug, :sector, :description
+
+  def initialize(slug, sector, description)
     @slug = slug
     @sector = sector
+    @description = description
   end
 
-  def id
-    @category_id ||= CATEGORIES.
-      map(&:first).
-      index(@slug)
-  end
+  #def id
+    #@category_id ||= CATEGORIES.
+      #map(&:first).
+      #index(@slug)
+  #end
 
   def selector
-    "#{name} - #{sector}"
-  end
-
-  def name
-    @slug.humanize
+    "#{description} - #{sector}"
   end
 
   def sector
@@ -33,7 +44,7 @@ class BuildingCategory
 
   def self.all
     CATEGORIES.map do |parts|
-      self.new(parts[0], parts[1])
+      self.new(*parts)
     end
   end
 
